4d5c800b0b38ab67893f65b0abf7dff69518516f
Pierre Quentel <pierre.quentel@gmail.com>
2014-10-17 10:39:01 +0200

    More content ; server points to the presentation page

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import sys
+from webbrowser import open_new_tab
 
 if sys.version < '3':
    import BaseHTTPServer as server
@@ -11,4 +12,6 @@ server_address = ('', 8001)
 handler = CGIHTTPRequestHandler
 httpd = server.HTTPServer(server_address, handler)
 print("server running on port %s" % server_address[1])
+open_new_tab("http://localhost:{}/brython_pycon.html".format(server_address[1]))
+
 httpd.serve_forever()
